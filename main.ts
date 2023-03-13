0ee6399459301a03bd2cb0154ca1dcfce0bcf956
Mes2-2 <122781305+Mes2-2@users.noreply.github.com>
2023-03-13 10:21:40 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.clearScreen()
+basic.forever(function () {
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
